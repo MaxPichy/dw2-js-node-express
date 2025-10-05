42f7c631c4d47519b002dbf6d0c1a73d56efaa43
MaxPichy <camilamachadodesouza77@gmail.com>
2025-10-04 23:43:49 -0300

    @MaxPichy

--- Node.js/_exercicio02/models/Intruso.js
+++ Node.js/_exercicio02/models/Intruso.js
@@ -2,31 +2,31 @@ import nodemon from 'nodemon';
 
 const Intruso = [
     {
-        foto: '/public/imgs/aang.png',
+        foto: 'aang.png',
         nome: 'Aang',
         habilidade: 'Dobra de Ar, Água, Terra e Fogo',
         nacao: 'Templo do Ar do Sul'
     },
     {
-        foto: '/public/imgs/katara.png',
+        foto: 'katara.png',
         nome: 'Katara',
         habilidade: 'Dobra Água e Sangue',
         nacao: 'Tribo da Água do Sul'
     },
     {
-        foto: '/public/imgs/korra.png',
+        foto: 'korra.png',
         nome: 'Korra',
         habilidade: 'Dobra de Água, Terra, Fogo, Ar e Metal',
         nacao: 'Tribo da Água do Sul'
     },
     {
-        foto: '/public/imgs/toph.png',
+        foto: 'toph.png',
         nome: 'Toph',
         habilidade: 'Dobra de Terra e Metal',
         nacao: 'Nação da Terra'
     },
     {
-        foto: '/public/imgs/zuko.png',
+        foto: 'zuko.png',
         nome: 'Zuko',
         habilidade: 'Dobra de Fogo e Raio',
         nacao: 'Nação do Fogo'
