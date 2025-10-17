727987abf7cb67123af46392b275872834d683ad
MaxPichy <camilamachadodesouza77@gmail.com>
2025-10-16 22:42:40 -0300

    @MaxPichy

--- Node.js/_aulas/nodejs-aula-05-SistemaLoja/controllers/PedidosController.js
+++ Node.js/_aulas/nodejs-aula-05-SistemaLoja/controllers/PedidosController.js
@@ -16,4 +16,63 @@ router.get("/pedidos", function (req, res) {
     });
 });
 
+router.post("/pedidos/new", (req, res) => {
+  const numero = req.body.numero;
+  const valorString = req.body.valor;
+  const valor = parseFloat(valorString);
+
+  Pedido.create({
+    numero: numero,
+    valor: valor
+  }).then(() => {
+    res.redirect("/pedidos");
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
+router.get("/pedidos/delete/:id", (req, res) => {
+  const id = req.params.id;
+
+  Pedido.destroy({
+    where: {
+      id: id
+    }
+  }).then(() => {
+    res.redirect("/pedidos");
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
+router.get("/pedidos/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  Pedido.findByPk(id).then(pedido => {
+    res.render("pedidoEdit", {
+      pedido: pedido
+    });
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
+router.post("/pedidos/update", (req, res) => {
+  const id = req.body.id;
+  const numero = req.body.numero;
+  const valorString = req.body.valor;
+  const valor = parseFloat(valorString);
+
+  Pedido.update({
+    numero: numero,
+    valor: valor
+  },
+  {where: {id: id}}
+  ).then(() => {
+    res.redirect("/pedidos");
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
 export default router;
